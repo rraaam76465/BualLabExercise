057a45c00fd4ab5adf8b7c1df1ab937d66d51573
EDRICK MAR BUAL <emobual2019@plm.edu.ph>
2023-09-19 15:32:49 +0800

    #update

--- BualLabExercise/Finals.vb
+++ BualLabExercise/Finals.vb
@@ -4,11 +4,7 @@
     Private Sub MidtermMenuForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         TextBox7.Text = MidtermGradeValue.ToString("F2")
     End Sub
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-
-
-    End Sub
 
 
 End Class
